99dd377c1b1c0cd41705bbdc0ce6100d8914ed4e
Kabir Nagpal <149412164+KabirNagpallll@users.noreply.github.com>
2023-12-31 02:34:36 +0530

    Update RaceCondition.sol

--- Vulnerabilities/RaceConditionAttack/RaceCondition.sol
+++ Vulnerabilities/RaceConditionAttack/RaceCondition.sol
@@ -1,1 +1,18 @@
+pragma solidity ^0.8.17;
+
+contract TokenSwap {
+   mapping(address => uint256) public balances;
+
+   function deposit(uint256 amount) public {
+       require(amount > 0, "Amount must be greater than 0");
+       balances[msg.sender] += amount;
+   }
+
+   function swapTokens(address recipient, uint256 amount) public {
+       require(balances[msg.sender] >= amount, "Not enough tokens");
+       require(recipient != address(0), "Invalid recipient");
+       balances[msg.sender] -= amount;
+       balances[recipient] += amount;
+   }
+}
 
