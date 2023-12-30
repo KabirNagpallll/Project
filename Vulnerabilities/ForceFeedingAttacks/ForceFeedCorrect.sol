9f7e3618f646c2b7664ffe86211c100fe2004ee8
Kabir Nagpal <149412164+KabirNagpallll@users.noreply.github.com>
2023-12-31 02:27:32 +0530

    Update ForceFeedCorrect.sol

--- Vulnerabilities/ForceFeedingAttacks/ForceFeedCorrect.sol
+++ Vulnerabilities/ForceFeedingAttacks/ForceFeedCorrect.sol
@@ -1,1 +1,16 @@
+pragma solidity ^0.8.17;
+
+import "./RewardVault.sol";
+
+contract Attacker {
+  RewardVault public vault;
+
+  constructor(RewardVault _vault) {
+      vault = _vault;
+  }
+
+  receive() external payable {
+      vault.deposit{value: msg.value}();
+  }
+}
 
