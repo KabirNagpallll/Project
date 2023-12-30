c20ec4e111de90bee8d73ea25fb68b4a4922b5cb
Kabir Nagpal <149412164+KabirNagpallll@users.noreply.github.com>
2023-12-31 02:26:37 +0530

    Update ForceFeeding.sol

--- Vulnerabilities/ForceFeedingAttacks/ForceFeeding.sol
+++ Vulnerabilities/ForceFeedingAttacks/ForceFeeding.sol
@@ -1,1 +1,22 @@
+pragma solidity ^0.8.17;
 
+contract RewardVault {
+  uint256 public totalDeposited = 0;
+  uint256 public constant LIMIT = 1 ether;
+  address public lastDepositor;
+
+  function deposit() public payable {
+      require(totalDeposited + msg.value <= LIMIT, "Limit reached");
+      totalDeposited += msg.value;
+      lastDepositor = msg.sender;
+  }
+
+  function withdrawReward() public {
+      require(lastDepositor == msg.sender, "Only the last depositor can withdraw");
+      require(totalDeposited > LIMIT, "Limit not reached yet");
+      uint256 reward = totalDeposited - LIMIT;
+      totalDeposited -= reward;
+      (bool success, ) = msg.sender.call{value: reward}("");
+      require(success, "Transfer failed");
+  }
+}
