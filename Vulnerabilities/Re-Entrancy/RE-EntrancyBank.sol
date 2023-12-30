797c951a158d56cf83a9fb0568817eb9ef944505
Kabir Nagpal <149412164+KabirNagpallll@users.noreply.github.com>
2023-12-31 02:39:37 +0530

    Update RE-EntrancyBank.sol

--- Vulnerabilities/Re-Entrancy/RE-EntrancyBank.sol
+++ Vulnerabilities/Re-Entrancy/RE-EntrancyBank.sol
@@ -1,1 +1,17 @@
+pragma solidity ^0.8.17;
+
+contract Bank {
+  mapping(address => uint256) public balances;
+
+  function deposit() public payable {
+      balances[msg.sender] += msg.value;
+  }
+
+  function withdraw(uint256 amount) public {
+      require(balances[msg.sender] >= amount, "Not enough balance");
+      (bool success, ) = msg.sender.call{value: amount}("");
+      require(success, "Transfer failed");
+      balances[msg.sender] -= amount;
+  }
+}
 
