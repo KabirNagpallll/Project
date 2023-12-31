f1557a67112ca72dc7afef192228c12216274008
Kabir Nagpal <149412164+KabirNagpallll@users.noreply.github.com>
2023-12-31 14:27:02 +0530

    Update UncheckedExternalCalls.sol

--- Vulnerabilities/UncheckedExternalCalls/UncheckedExternalCalls.sol
+++ Vulnerabilities/UncheckedExternalCalls/UncheckedExternalCalls.sol
@@ -1,1 +1,14 @@
+contract VulnerableContract {
+   address payable public owner;
+
+   constructor() {
+       owner = msg.sender;
+   }
+
+   function withdrawAll() public {
+       require(msg.sender == owner);
+       owner.transfer(address(this).balance);
+      //Edit balance baadmein;
+   }
+}
 
