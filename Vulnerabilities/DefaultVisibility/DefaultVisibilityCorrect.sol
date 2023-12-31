00027c306095d246761d2efdfd823bbcde1533c1
Kabir Nagpal <149412164+KabirNagpallll@users.noreply.github.com>
2023-12-31 12:19:57 +0530

    Update DefaultVisibilityCorrect.sol

--- Vulnerabilities/DefaultVisibility/DefaultVisibilityCorrect.sol
+++ Vulnerabilities/DefaultVisibility/DefaultVisibilityCorrect.sol
@@ -1,1 +1,9 @@
+pragma solidity ^0.5.0;
 
+contract MyContract {
+   uint256 private secretNumber;
+
+   function setSecretNumber(uint256 _secretNumber) private {
+       secretNumber = _secretNumber;
+   }
+}
